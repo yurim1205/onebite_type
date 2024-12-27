d03ca76f9d9e61d3761a025cbbe3b448bca3d983
yurim1205 <eoulim3237@naver.com>
2024-12-28 01:41:40 +0900

    객체

---
+++ section2/src/chapter3.ts
@@ -0,0 +1,36 @@
+// object
+let user:{
+    id: number;
+    name: string;
+} = {
+    id:1,
+    name: "이유림",
+};
+
+let dog:{
+    name: string;
+    color: string;
+} = {
+    name: "뽀야",
+    color: "brown",
+}
+
+// 선택적 프로퍼티
+let user2:{
+    id?: number;
+    name: string;
+} = {
+    id:1,
+    name: "이유림",
+};
+
+user2 = {
+    id: 1,
+    name: "이유림",
+};
+
+let config:{
+    apiKey: string; // apiKey는 수정되면 안 되는 프로퍼티이므로 readonly를 붙여야함
+} = {
+    apiKey: "MY API KEY",
+};

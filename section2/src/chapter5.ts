08b2872d52475d1151ed7a1045bfb3a9d5f4b175
yurim1205 <eoulim3237@naver.com>
2024-12-30 22:36:17 +0900

    enum 타입

---
+++ section2/src/chapter5.ts
@@ -0,0 +1,31 @@
+// enum 타입
+// 여러가지 값들에 각각 이름을 부여해 열거해두고 사용하는 타입
+
+enum Role {
+    ADMIN = 0,
+    USER = 1,
+    GUEST = 2,
+}
+
+enum Language {
+    korean = "ko",
+    english = "en"
+}
+
+const user1 = {
+    name: "이유림",
+    role: Role.ADMIN,
+    language: Language.korean
+}
+
+const user2 = {
+    name: "박",
+    role: Role.USER
+}
+
+const user3 = {
+    name: "김",
+    role: Role.GUEST
+}
+
+console.log(user1,user2,user3);
